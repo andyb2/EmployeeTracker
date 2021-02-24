98e32aaf7ad7b1aadd7e9cc69b5519400affc1ed
AMB <a.michael.boyle@gmail.com>
2021-02-24 15:55:31 -0500

    Added switch statement with empty function calls

--- prompt.js
+++ prompt.js
@@ -1,28 +1,55 @@
 const inquirer = require('inquirer');
 const db = require('./app/connection')('employee_db', 'rootroot');
 const cTable = require('console.table');
-
-
-const questionList = [
-    {
-        type: 'list',
-        message: 'What would you like to do?',
-        name: 'firstChoice',
-        choices: ['View all Employees', 'View all employees by Department', 'View all employees by Role', '', '']
-    }
-]
+const { ADDRGETNETWORKPARAMS } = require('dns');
+const { allowedNodeEnvironmentFlags } = require('process');
 
 
 async function questionsPrompt() {
-    const inqPrompt = await inquirer
-        .prompt(questionList);
-        console.log(inqPrompt.firstChoice)
-    if(inqPrompt.name === 'Add employee'){
-        addEmployee();
+    const questionList = await inquirer.prompt([
+        {
+            type: 'list',
+            message: 'What would you like to do?',
+            name: 'firstChoice',
+            choices: ['View all Employees', 'View all employees by Department', 'View all employees by Role', 'Add employee', 'Update employee role', 'Add role', 'Add department', 'Exit Application']
+        }
+    ])
+
+    console.log(introQuestion.firstChoice)
+    
+    switch(questionList.firstChoice){
+        case 'View all Employees':
+            viewAllEmployees();
+        break;
+
+        case 'View all employees by Department':
+            viewAllEmpDepartments();
+        break;
+
+        case 'View all employees by Role':
+            viewAllEmpRoles();
+        break;
+
+        case 'Add employee':
+            addEmployee();
+        break;
+
+        case 'Update employee role':
+            updateEmpRole();
+        break;
+
+        case 'Add role':
+            addRole();
+        break;
+
+        case 'Add department':
+            addDepartment();
+        default:
+            process.exit
     }
 }
 questionsPrompt();
 
 async function addEmployee(){
-
+    console.log('tested')
 }
