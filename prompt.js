9567692b940c95284984cdc254845c4862a3d2ba
AMB <a.michael.boyle@gmail.com>
2021-02-25 17:26:04 -0500

    Roles now show a list of departments

--- prompt.js
+++ prompt.js
@@ -13,32 +13,33 @@ async function questionsPrompt() {
             name: 'firstChoice',
             choices: ['View all Employees', 'View all employees by Department', 'View all employees by Role', 'Add employee', 'Update employee role', 'Add role', 'Add department', 'Exit Application']
         }
-    ])
+    ]);
 
-    switch(questionList.firstChoice){
+
+    switch (questionList.firstChoice) {
         case 'View all Employees':
             viewAllEmployees();
-        break;
+            break;
 
         case 'View all employees by Department':
             viewAllEmpDepartments();
-        break;
+            break;
 
         case 'View all employees by Role':
             viewAllEmpRoles();
-        break;
+            break;
 
         case 'Add employee':
             addEmployee();
-        break;
+            break;
 
         case 'Update employee role':
             updateEmpRole();
-        break;
+            break;
 
         case 'Add role':
             addRole();
-        break;
+            break;
 
         case 'Add department':
             addDepartment();
@@ -48,47 +49,66 @@ async function questionsPrompt() {
 }
 questionsPrompt();
 
-async function viewAllEmployees(){
+async function viewAllEmployees() {
+    let allEmp = db.query('SELECT * FROM employee') 
+    console.table(allEmp)
+}
+async function viewAllEmpDepartments() {
     console.log('tested')
 }
-async function viewAllEmpDepartments(){
-    console.log('tested')
+async function viewAllEmpRoles() {
+    // let viewRoles = db.query('S')
 }
-async function viewAllEmpRoles(){
-    console.log('tested')
-}
-async function addEmployee(){
-    let roleDb = db.query('SELECT * FROM roles')
+async function addEmployee() {
+  
     const addEmpQuestion = await inquirer.prompt([
-        {message: 'What is the employees first name?', name: 'firstName'},
-        {message: 'What is the employees last name?', name: 'lastName'},
-        {type: 'list', message: 'What is the employees role?', name: 'role', choices: [`${roleDb}`]}
+        { message: 'What is the employees first name?', name: 'firstName' },
+        { message: 'What is the employees last name?', name: 'lastName' },
+        { type: 'list', message: 'What is the employees role?', name: 'role', choices: [`${roleQuery}`] }
     ]);
+    console.log(`[Employee Added]: ${addEmpQuestion.firstName}, ${addEmpQuestion.lastName}`)
+
+    let addEmp = db.query(`INSERT INTO employee (first_name, last_name) VALUES ('${firstName}', '${lastName})`)
+    let roleQuery = db.query(`SELECT * FROM role`)
 }
 
-async function updateEmpRole(){
+async function updateEmpRole() {
     console.log('tested')
 }
+// department.id, department.nameDepart
+async function addRole() {
+    let deptQuery = await db.query(`SELECT * FROM department;`);
+    //dList=[{name:,value:},{}]
+    const dList = deptQuery.map(({id, nameDepart})=>({
+        name: nameDepart,
+        value: id,
+    }));
+    console.log(dList)
 
-async function addRole(){
     const addRoleQuestion = await inquirer.prompt([
-        {message: 'What is the title of the role?', name: 'roleName'},
-        {message: 'What is the salary of this role?', name: 'roleSalary'},
+        { message: 'What is the title of the role?', name: 'title' },
+        { message: 'What is the salary of this role?', name: 'salary' },
+        { type: 'list', message: 'What is the department?', name: 'department_id', choices: dList },
     ]);
-    if(addRoleQuestion !== ''){
-        let roleDb = await db.query(`INSERT INTO roles (title, salary) VALUES ('${addRoleQuestion.roleName}', '${addRoleQuestion.roleSalary}');`)
-        console.log(`[Role Added]: ${addRoleQuestion.roleName}, [Salary]: $${addRoleQuestion.roleSalary} per year`)
-    }
+        
+        return await db.query(`INSERT INTO roles (title, salary, department_id) VALUES ('${addRoleQuestion.title}', '${addRoleQuestion.salary}', ${addRoleQuestion.department_id});`)
+        // console.log(`[Role Added]: ${addRoleQuestion.roleName}, [Salary]: $${addRoleQuestion.roleSalary} per year`)
+
+    questionsPrompt();
 }
 
-async function addDepartment(){
+async function addDepartment() {
     const addDeptQuestion = await inquirer.prompt([
-        {message: 'What is the department name?', name: 'deptName'},
+        { message: 'What is the department name?', name: 'deptName' },
     ]);
-    if(addDeptQuestion.deptName !== ''){
+    if (addDeptQuestion.deptName !== '') {
         let deptDb = await db.query(`INSERT INTO department (nameDepart) VALUES ('${addDeptQuestion.deptName}');`);
         console.log(`[Department]: ${addDeptQuestion.deptName} created`)
     }
     console.log(await db.query(`SELECT * FROM department;`));
     questionsPrompt();
 }
+
+// async function retDeptNames(){
+//     return await db.query(`SELECT * FROM department`);
+// }
