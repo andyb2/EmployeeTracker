823220d6b6477e0bc52cd1393747696692a843e0
AMB <a.michael.boyle@gmail.com>
2021-03-09 15:07:03 -0500

    seeds added

---
+++ Assets/seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO department (nameDepart) VALUES ('Software Development');
+INSERT INTO roles (title, salary, department_id) VALUES ('Engineer', 50000, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Dennis', 'Michaels', 1, NULL);
