9f4e9b91f5623448d376fa67429d8bace37a2626
AMB <a.michael.boyle@gmail.com>
2021-03-09 15:06:47 -0500

    old version of schema... forgot to add new one

--- Assets/schema.sql
+++ Assets/schema.sql
@@ -2,27 +2,29 @@ CREATE DATABASE employee_db;
 
 USE employee_db;
 
+CREATE TABLE department(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nameDepart VARCHAR(30)
+);
+
+CREATE TABLE roles(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL(10, 2),
+    department_id INT,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+);
+
 CREATE TABLE employee(
     id INT PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
     INDEX role_ind (role_id),
-    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE,
     manager_id INT,
     INDEX man_ind (manager_id),
     CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
 
-CREATE TABLE roles(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    title VARCHAR(30),
-    salary DECIMAL(10, 4),
-    department_id INT,
-    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
-);
 
-CREATE TABLE department(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    nameDepart VARCHAR(30)
-);
